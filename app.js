e9bdb25082feb7f5abd915e7abf0ff9c7c130c03
22CS036dhruvimahale <22cs036@charusat.edu.in>
2024-03-13 11:30:46 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,4 @@
+document.getElementById('search-button').addEventListener('click', function() {
+  var query = document.getElementById('search-input').value;
+  console.log("You searched for: " + query);
+});
---
+++ src/router.tsx
@@ -0,0 +1,25 @@
+import { Route, Routes } from "react-router-dom";
+import { ReactElement } from "react";
+import UserSettings from "./pages/UserSettings";
+import Categories from "./pages/Categories";
+import NotFound from "./pages/NotFound";
+import AddTask from "./pages/AddTask";
+import Home from "./pages/Home";
+import TaskDetails from "./pages/TaskDetails";
+import SharePage from "./pages/Share";
+
+const AppRouter = (): ReactElement => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/task/:id" element={<TaskDetails />} />
+      <Route path="/share" element={<SharePage />} />
+      <Route path="/add" element={<AddTask />} />
+      <Route path="/user" element={<UserSettings />} />
+      <Route path="/categories" element={<Categories />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
+
+export default AppRouter;
